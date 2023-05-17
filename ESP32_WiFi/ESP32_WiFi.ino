f24a1362fe77ae35b610e5d088feffe4c6d3bb1e
Utkarsh Roy <81402963+utkarsh-iit@users.noreply.github.com>
2023-05-17 18:13:57 +0530

    Create ESP32_WiFi.ino

---
+++ ESP32_WiFi/ESP32_WiFi.ino
@@ -0,0 +1,21 @@
+#include <WiFi.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.begin("ssid", "password");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("..");
+    delay(200);
+  }
+  Serial.println();
+  Serial.println("ESP32 is Connected!");
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
